993017c5ba7c1fadc6200ac3faaa1c4b58c593e2
faishalam <faishalabdulmajid@gmail.com>
2025-07-02 14:38:16 +0700

    done migration

--- server/models/soplibrary.js
+++ server/models/soplibrary.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class SopLibrary extends Model {
     /**
@@ -11,23 +9,142 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      SopLibrary.belongsTo(models.User, { foreignKey: "userId" });
+      SopLibrary.belongsTo(models.Category, { foreignKey: "categoryId" });
     }
   }
-  SopLibrary.init({
-    userId: DataTypes.STRING,
-    categoryId: DataTypes.STRING,
-    business_process: DataTypes.STRING,
-    title: DataTypes.STRING,
-    effective_date: DataTypes.DATE,
-    document: DataTypes.STRING,
-    superior_1: DataTypes.STRING,
-    superior_2: DataTypes.STRING,
-    md: DataTypes.STRING,
-    status: DataTypes.STRING,
-    reasonRevise: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'SopLibrary',
-  });
+  SopLibrary.init(
+    {
+      userId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "User ID is required",
+          },
+          notEmpty: {
+            msg: "User ID is required",
+          },
+        },
+      },
+      categoryId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Category ID is required",
+          },
+          notEmpty: {
+            msg: "Category ID is required",
+          },
+        },
+      },
+      business_process: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Business process is required",
+          },
+          notEmpty: {
+            msg: "Business process is required",
+          },
+        },
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Title is required",
+          },
+          notEmpty: {
+            msg: "Title is required",
+          },
+        },
+      },
+      effective_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Effective Date is required",
+          },
+          notEmpty: {
+            msg: "Effective Date is required",
+          },
+        },
+      },
+      document: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Document is required",
+          },
+          notEmpty: {
+            msg: "Document is required",
+          },
+        },
+      },
+      superior_1: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Superior 1 is required",
+          },
+          notEmpty: {
+            msg: "Superior 1 is required",
+          },
+        },
+      },
+      superior_2: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "superior 2 is required",
+          },
+          notEmpty: {
+            msg: "superior 2 is required",
+          },
+        },
+      },
+      md: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "MD is required",
+          },
+          notEmpty: {
+            msg: "MD is required",
+          },
+        },
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "submitted",
+        validate: {
+          notNull: {
+            msg: "Status is required",
+          },
+          notEmpty: {
+            msg: "Status is required",
+          },
+        },
+      },
+      reasonRevise: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "SopLibrary",
+    }
+  );
   return SopLibrary;
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,17 +9,81 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Category, { foreignKey: "userId" });
+      User.hasMany(models.SopLibrary, { foreignKey: "userId" });
+      User.hasMany(models.InternalMemo, { foreignKey: "userId" });
     }
   }
-  User.init({
-    username: DataTypes.STRING,
-    nrp: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Username is required",
+          },
+          notEmpty: {
+            msg: "Username is required",
+          },
+        },
+      },
+      nrp: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {
+            msg: "NRP is required",
+          },
+          notEmpty: {
+            msg: "NRP is required",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Password is required",
+          },
+          notEmpty: {
+            msg: "Password is required",
+          },
+        },
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "user",
+        validate: {
+          notNull: {
+            msg: "Role is required",
+          },
+          notEmpty: {
+            msg: "Role is required",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {
+            msg: "Email is required",
+          },
+          notEmpty: {
+            msg: "Email is required",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
